90bff6a9412949845d74a55ff3355f0bfb489da3
jubo445 <juboraj335.ja@gmail.com>
2025-07-03 20:43:50 +0600

    tailwind added

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex flex-1 justify-center items-center bg-blue-400 ">
-      <Text className={'flex justify-center items-center text-5xl text-white '}>Welcome!</Text>
+    <View className="flex-1 justify-center items-center bg-light-100 ">
+      <Text className={'text-5xl text-dark-200 font-bold'}>Welcome!</Text>
     </View>
   );
 }
