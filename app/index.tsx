1a263eb9c00958e3d9d6d2cb746272dd3efe6867
jubo445 <juboraj335.ja@gmail.com>
2025-07-03 15:13:44 +0600

    project setup done

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 justify-center items-center bg-blue-400 ">
+      <Text className={'flex justify-center items-center text-5xl text-white '}>Welcome!</Text>
+    </View>
+  );
+}
