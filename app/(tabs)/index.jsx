b56852a56a052599c804f3ab339a194c4267d6cb
jubo445 <juboraj335.ja@gmail.com>
2025-07-08 17:44:49 +0600

    tab added

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-light-100 ">
       <Text className={'text-5xl text-dark-200 font-bold'}>Welcome!</Text>
+
+
     </View>
   );
 }
